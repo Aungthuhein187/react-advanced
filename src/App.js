722fc75ad34cda2a99f0b06e9ac5be22d130f87f
Aungthuhein187 <aungthuhein187@gmail.com>
2020-12-29 06:20:01 +0630

    enter

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React, { Component } from 'react';
 import MoviePage from './context/MoviePage';
 import UserContext from './context/userContext';
 import './App.css';
+import User from './hooks/User';
 
 class App extends Component {
   state = { user: { name: 'Aung' } };
   render() {
     return (
-      <UserContext.Provider value={this.state.user}>
-        <MoviePage />
-      </UserContext.Provider>
+      // <UserContext.Provider value={this.state.user}>
+      //   <MoviePage />
+      // </UserContext.Provider>
+      <User />
     );
   }
 }
--- src/hooks/User.jsx
+++ src/hooks/User.jsx
@@ -1,8 +1,28 @@
-import React, { useEffect } from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 function User(props) {
-  useEffect(() => {});
-  return <div></div>;
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    async function getUsers() {
+      const response = await axios(
+        'https://jsonplaceholder.typicode.com/users'
+      );
+      setUsers(response.data);
+    }
+
+    getUsers();
+  });
+  return (
+    <div>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default User;
